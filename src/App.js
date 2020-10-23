f8ebf62f1bcb1a186e26c19dc2df6b86f944ba1e
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-10-23 23:10:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+class App extends Component{
+  state={
+    c:0,
+  }
+  increase=()=>{
+      this.setState({c:this.state.c+1});
+  }
+  decrease=()=>{
+    if(this.state.c >= 1)
+      this.setState({c:this.state.c-1});
+    else
+      <p>Not below zero</p>
+  }
+  render(){
+    return <div>
+      <Navbar/>
+      <div className="bor">
+        <h3>{this.state.c}</h3>
+        <button onClick={this.increase}>Increase Count</button>
+        <button onClick={this.decrease} >Decrease Count</button>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+function Navbar(){
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href="#"><li>Contact Us</li></a>
+                </ul>
+            </nav>
+            <h1>Heading</h1>
+        </div>
+    )
+}
+
+export default Navbar;
